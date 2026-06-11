c3b978252f1fd82daa072d9831a7ed98e3301bdb
petert97-arch <petert97@gmail.com>
2026-06-11 11:07:42 -0500

    Update 02_Data_Cleaning.sql

--- 02_Data_Cleaning.sql
+++ 02_Data_Cleaning.sql
@@ -1,1 +1,17 @@
+-- Validate customer dimension mapping
+SELECT product, sales.customer_name, price
+FROM sales
+JOIN dim_customer
+ON sales.customer_name = dim_customer.customer_name;
 
+-- Validate fact table to product dimension relationship
+SELECT *
+FROM fact_order_details
+JOIN dim_product
+ON fact_order_details.product_id = dim_product.product_id;
+
+-- Check for unmatched records between fact and dimension tables
+SELECT *
+FROM dim_product
+FULL JOIN fact_order_details
+ON fact_order_details.product_id = dim_product.product_id;
